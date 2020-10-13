e66d7e8bcd7bb677706e9213933ea85264ec22ff
writeonkd <writeonkd@gmail.com>
2020-10-13 22:32:40 +0530

    add test cases

--- src/test/java/com/sample/www/TC02Flights.java
+++ src/test/java/com/sample/www/TC02Flights.java
@@ -20,24 +20,13 @@ public class TC02Flights extends TestBase {
                 .assignAuthor("Kunal");
         logger.info("Verify Page Results");
 
-        List<WebElement> elementList = driver.findElements(By.xpath("//span[@data-cy='finalPrice']"));
-        elementList.get(0).getText();
-
-        List<String> originalList = new ArrayList();
-        for (int i = 0; i < elementList.size(); i++) {
-            originalList.add(elementList.get(i).getText());
-            test.log(Status.INFO, "list " + elementList.get(i).getText());
-            logger.info("list" + elementList.get(i).getText());
-        }
-        List<String> tempList = originalList;
-        Collections.sort(tempList);
-
+        validateTheCost();
     }
 
     @Test
     public void tc02VerifyApplyFilters() {
 
-        test = extent.createTest("Verify Page Results", "Case 4: Apply filters in the left side still the page results are ordered in decreasing order of cost")
+        test = extent.createTest("Verify Apply Filters", "Case 4: Apply filters in the left side still the page results are ordered in decreasing order of cost")
                 .assignCategory("Functional_testcase")
                 .assignAuthor("Kunal");
         logger.info("Verify Apply Filters");
@@ -53,5 +42,40 @@ public class TC02Flights extends TestBase {
         webdriver.clickOnButton(Flights.airLinesFilterAirIndia);
         test.log(Status.INFO, "Apply airLines filter Air India");
         logger.info("Apply airLines filter Air India");
+
+        validateTheCost();
     }
+
+    public void validateTheCost() {
+
+        List<WebElement> elementList = driver.findElements(By.xpath("//span[@data-cy='finalPrice']"));
+        elementList.get(0).getText();
+
+        List<String> originalList = new ArrayList();
+        for (int i = 0; i < elementList.size(); i++) {
+            originalList.add(elementList.get(i).getText());
+            test.log(Status.INFO, "list " + elementList.get(i).getText());
+            logger.info("list" + elementList.get(i).getText());
+        }
+        List<String> tempList = originalList;
+        Collections.sort(tempList);
+
+        boolean isValid = getResult(originalList, tempList);
+
+        test.log(Status.INFO, isValid ? "Page results are in decreasing order" : "Page results are not ordered in decreasing order");
+        logger.info(isValid ? "Page results are in decreasing order" : "Page results are not ordered in decreasing order");
+        System.out.println(isValid ? "Page results are in decreasing order" : "Page results are not ordered in decreasing order");
+    }
+
+    private boolean getResult(List<String> originalList, List<String> tempList) {
+
+        for (int i = 0; i < tempList.size(); i++) {
+            if (!tempList.get(i).equalsIgnoreCase(originalList.get(i))) {
+                System.out.println("Error");
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
